9a18fdeef2267c956af3ab706196b2a44a7a040a
Carlos7847 <carlitosmp1597g@gmail.com>
2024-07-16 00:42:25 -0500

    se agrega comentario

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = process.env.VUE_APP_API_BASE_URL || 'http://localhost:3000/api/v1'
 
-
+// se crea configuración global para peticiones axios
 const apiClient = axios.create({
     baseURL: baseURL,
     headers: {
